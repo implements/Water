11bcef400f98fe8e76ea2af2cd7f75c7f2c9359a
Thoai-Lam Nguyen <thoailam@gmail.com>
2018-01-18 19:29:43 +0100

    PoC.

--- water/ViewController.swift
+++ water/ViewController.swift
@@ -7,19 +7,45 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        authorize()
+        createNotifications()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func authorize() {
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: .alert) { (granted, error) in
+            print("Permission Granted!")
+        }
+    }
+    
+    func createNotifications() {
+        let content = UNMutableNotificationContent()
+        content.title = NSString.localizedUserNotificationString(forKey: "Drink Water!", arguments: nil)
+        //content.body = NSString.localizedUserNotificationString(forKey: "Nu!", arguments: nil)
+        
+        for index in 9...14 {
+            var date = DateComponents()
+            date.hour = index
+            date.minute = 00
+            let trigger = UNCalendarNotificationTrigger(dateMatching: date, repeats: true)
+            let request = UNNotificationRequest(identifier: String(index), content: content, trigger: trigger)
+            let center = UNUserNotificationCenter.current()
+            center.add(request) { (error: Error?) in
+                if let theError = error {
+                    print(theError.localizedDescription)
+                }
+            }
+        }
+    }
 }
-

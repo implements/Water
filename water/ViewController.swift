c893d42757116bb8f0449cff9031653147b1f30e
Thoai-Lam Nguyen <thoailam@gmail.com>
2018-02-04 00:19:24 +0100

    Fixed bug with notification identifiers.

--- water/ViewController.swift
+++ water/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  water
-//
-//  Created by Thoai-Lam Nguyen on 18/01/2018.
-//  Copyright © 2018 Thoai-Lam Nguyen. All rights reserved.
-//
-
 import UIKit
 import UserNotifications
 
@@ -14,15 +6,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let launchedBefore = UserDefaults.standard.bool(forKey: "launchedBefore")
-        if launchedBefore  {
-            print("Startup")
-        } else {
-            print("First Launch")
-            authorize()
-            createNotifications()
-            UserDefaults.standard.set(true, forKey: "launchedBefore")
+        if !UserDefaults.standard.bool(forKey: "launchedBefore")  {
+            firstLaunch()
         }
+        UNUserNotificationCenter.current().getPendingNotificationRequests(completionHandler: { requests in
+            for request in requests {
+                print(request)
+            }
+        })
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,28 +21,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func firstLaunch() {
+        print("First Launch")
+        authorize()
+        createNotifications()
+        UserDefaults.standard.set(true, forKey: "launchedBefore")
+    }
+    
     func authorize() {
-        let center = UNUserNotificationCenter.current()
-        center.requestAuthorization(options: .alert) { (granted, error) in
+        UNUserNotificationCenter.current().requestAuthorization(options: .alert) { (granted, error) in
             print("Permission Granted!")
         }
     }
     
     func createNotifications() {
+        
         let content = UNMutableNotificationContent()
         let center = UNUserNotificationCenter.current()
+        
         center.removeAllPendingNotificationRequests()
         content.title = NSString.localizedUserNotificationString(forKey: "Drik et glas vand.", arguments: nil)
-        //content.body = NSString.localizedUserNotificationString(forKey: "Nu!", arguments: nil)
         
         for day in 2...6 {
-            for hourUnit in 9...14 {
+            for hour in 9...14 {
                 var date = DateComponents()
-                date.weekday = day;
-                date.hour = hourUnit
-                date.minute = 00
+                date.weekday = day
+                date.hour = hour
+                date.minute = 0
                 let trigger = UNCalendarNotificationTrigger(dateMatching: date, repeats: true)
-                let request = UNNotificationRequest(identifier: String(day + hourUnit), content: content, trigger: trigger)
+                let request = UNNotificationRequest(identifier: "DAY" + String(day) + "HOUR" + String(hour), content: content, trigger: trigger)
                 center.add(request) { (error: Error?) in
                     if let theError = error {
                         print(theError.localizedDescription)
